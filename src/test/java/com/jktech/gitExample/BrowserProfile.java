35eff59f50ce7e9f38fe69e7a3b807d5ed0182be
“Ragavendran” <raghavendran.v@jktech.com>
2022-07-18 12:44:34 +0530

    Git Practise 1

---
+++ src/test/java/com/jktech/gitExample/BrowserProfile.java
@@ -0,0 +1,16 @@
+package com.jktech.gitExample;
+
+
+public class BrowserProfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100;
+		int b = 200;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
